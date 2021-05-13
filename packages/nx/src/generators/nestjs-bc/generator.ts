ff9f29dac20180d4b9c0c27b05d077d963eb56d2
SirReiva <gjjavicoru@hotmail.com>
2021-05-13 10:43:04 +0200

    nestjs bc - project selector

--- packages/nx/src/generators/nestjs-bc/generator.ts
+++ packages/nx/src/generators/nestjs-bc/generator.ts
@@ -1,8 +1,24 @@
 import type { Tree } from '@nrwl/devkit';
+import prompts from 'prompts';
+import { Tags } from '../../constants/tags.enum';
+import { findProjectsByTag } from '../../utils/find-projects-by-tag';
 import { addFiles, normalizeOptions } from './lib';
 import type { NestJsBcCLIOptions } from './schema';
 
 export default async (tree: Tree, options: NestJsBcCLIOptions) => {
+	const nestProjects = findProjectsByTag(tree, Tags.NESTJS_APP);
+	const targetProject = (
+		await prompts({
+			type: 'select',
+			name: 'value',
+			message: 'NestJs base project name',
+			choices: nestProjects.map(p => ({ title: p, value: p })),
+		})
+	).value;
+	options = {
+		...options,
+		baseProjectName: targetProject,
+	};
 	const normalizedOptions = normalizeOptions(tree, options);
 	if (
 		tree.exists(
