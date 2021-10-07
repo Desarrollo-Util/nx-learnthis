0638376da392a28ce9bf5554b29abb08e672c539
Pablo Castellanos <pablocd168@gmail.com>
2021-10-07 15:55:59 +0200

    Responsive added XL

--- libs/tailwind-utils/src/lib/generate-responsive-screens.ts
+++ libs/tailwind-utils/src/lib/generate-responsive-screens.ts
@@ -3,20 +3,23 @@
  * @param xs XS max resolution in pixels
  * @param sm SM max resolution in pixels
  * @param md MD max resolution in pixels
+ * @param lg LG max resolution in pixels
  * @returns Screens property
  */
 export const generateResponsiveScreens = (
 	xs: number,
 	sm: number,
-	md: number
+	md: number,
+	lg: number
 ) => {
 	if (
 		typeof xs !== 'number' ||
 		typeof sm !== 'number' ||
-		typeof md !== 'number'
+		typeof md !== 'number' ||
+		typeof lg !== 'number'
 	)
 		throw new Error(
-			'generateResponsiveScreens: All breakpoints "xs", "sm" and "md" must be provided'
+			'generateResponsiveScreens: All breakpoints "xs", "sm", "md" and "lg" must be provided'
 		);
 
 	if (!Number.isInteger(xs) || !Number.isInteger(sm) || !Number.isInteger(md))
@@ -29,10 +32,14 @@ export const generateResponsiveScreens = (
 		sm: { min: `${xs + 1}px`, max: `${sm}px` },
 		md: { min: `${sm + 1}px`, max: `${md}px` },
 		lg: { min: `${md + 1}px` },
+		xl: { min: `${lg + 1}px`},
 		xssm: { max: `${sm}px` },
 		xsmd: { max: `${md}px` },
+		xslg: { max: `${lg}px` },
 		smmd: { min: `${xs + 1}px`, max: `${md}px` },
-		smlg: { min: `${xs + 1}px` },
-		mdlg: { min: `${sm + 1}px` },
+		smlg: { min: `${xs + 1}px`, max: `${lg}px` },
+		smxl: { min: `${xs + 1}px` },
+		mdlg: { min: `${sm + 1}px`, max: `${lg}px` },
+		mdxl: { min: `${sm + 1}px` },
 	};
 };
